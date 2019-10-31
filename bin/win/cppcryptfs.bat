b39cceb976b1352256147e9af4f172ccb5ab3cdf
otter.pro <coconutstudio@yahoo.com>
2019-10-30 23:38:36 -0400

    create cppcryptfs.bat

---
+++ bin/win/cppcryptfs.bat
@@ -0,0 +1,23 @@
+@rem ====================================================
+@rem run cppcryptfs in background and give it time to start up
+@rem ====================================================
+
+start cppcryptfs.exe --tray
+timeout /t 1 >nul
+
+@rem ====================================================
+@rem Mount drive k: 
+@rem ====================================================
+
+cppcryptfs.exe --mount=o:\HD0\_he0 --drive=k  --saved-password --tray  --exit
+
+@rem ====================================================
+@rem Mount drive V:
+@rem ====================================================
+
+@rem cppcryptfs.exe --mount=d:\TestCppCryptFS2 --drive=v --password=PASSWORD --tray  --exit
+
+@rem ====================================================
+@rem Run any command with the mounted drives
+@rem ====================================================
+

709f899405bb5c9525b41a84d98421248ff98a2b
otter.pro <coconutstudio@yahoo.com>
2019-11-02 17:59:06 -0400

    rename cpp cryptfs

--- bin/win/mount_cryptfs.bat
+++ bin/win/mount_cryptfs.bat
@@ -2,16 +2,16 @@
 @rem run cppcryptfs in background and give it time to start up
 @rem ====================================================
 
-@rem start cppcryptfs.exe --tray
-@rem timeout /t 1 >nul
+start cppcryptfs.exe --tray
+timeout /t 1 >nul
 
 @rem ====================================================
 @rem Mount drive k: 
 @rem ====================================================
 
 @rem cppcryptfs.exe --mount=o:\HD0\_he0 --drive=k  --saved-password --tray  --exit
-@rem cppcryptfs --mount=o:\hd0\_he0 -P --drive=c:\_mount\he0 --tray --exit
-cppcryptfs --mount=o:\hd0\_he0 -P --drive=k --tray --exit
+cppcryptfs --mount=o:\hd0\_he0 -P --drive=c:\_mount\he0 --tray --exit
+@rem cppcryptfs --mount=o:\hd0\_he0 -P --drive=k --tray --exit
 
 @rem ====================================================
 @rem Mount drive V:

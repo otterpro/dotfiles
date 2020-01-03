f6935523c53ecd8a51e62876f45d9b29ff59418a
otter.pro <coconutstudio@yahoo.com>
2020-01-02 23:16:06 -0500

    update karabiner, zshrc, and add backup/backup_10.sh

---
+++ bin/backup/backup_10.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# copies encrypted doc.sparsebundle/ into 
+echo "======================"
+echo "doc.sparsebundle: "
+date "+%Y-%m-%d:%H:%M:%S"
+if [ -d /Volumes/10 ] && [ -d /Users/otter/Documents/doc.sparsebundle ] ; then
+	/usr/bin/rsync -av --del /Users/otter/Documents/doc.sparsebundle/ /Volumes/10/doc_backup/doc.sparsebundle 
+fi
+# if [ -d /Volumes/WD4T/HD0 ] && [ -d /Users/otter/Documents/doc.sparsebundle ] ; then
+# 	/usr/bin/rsync -av --del /Users/otter/Documents/doc.sparsebundle/ /Volumes/WD4T/HD0/doc_backup/doc.sparsebundle 
+# fi
+# if [ -d /Volumes/HD0/doc.sparsebundle ] && [ -d /Users/otter/Box\ Sync ] ; then
+# 	/usr/bin/rsync -av --del /Volumes/HD0/doc.sparsebundle/ /Users/otter/Box\ Sync/backups/doc.sparsebundle
+# fi

2d8f372d4456ac78934acd0ccf1c4a748aacf2e4
otter.pro <coconutstudio@yahoo.com>
2020-01-02 23:28:05 -0500

    add backup 10 to WD4T

--- bin/backup/backup_10.sh
+++ bin/backup/backup_10.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
-# copies encrypted doc.sparsebundle/ into 
 echo "======================"
-echo "doc.sparsebundle: "
+echo "backup_10.sh"
 date "+%Y-%m-%d:%H:%M:%S"
+# copies encrypted doc.sparsebundle/ into 
 if [ -d /Volumes/10 ] && [ -d /Users/otter/Documents/doc.sparsebundle ] ; then
 	/usr/bin/rsync -av --del /Users/otter/Documents/doc.sparsebundle/ /Volumes/10/doc_backup/doc.sparsebundle 
+fi
+# copies HDD "10" to WD4T
+if [ -d /Volumes/WD4T/HD0 ] && [ -d /Volumes/10 ] ; then
+	/usr/bin/rsync -av --del /Volumes/10/ /Volumes/WD4T/HD0 
 fi
 # if [ -d /Volumes/WD4T/HD0 ] && [ -d /Users/otter/Documents/doc.sparsebundle ] ; then
 # 	/usr/bin/rsync -av --del /Users/otter/Documents/doc.sparsebundle/ /Volumes/WD4T/HD0/doc_backup/doc.sparsebundle 

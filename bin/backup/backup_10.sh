ebe74f5cf0ad61432f49a1223be7729566327a01
otter.pro <coconutstudio@yahoo.com>
2020-01-03 12:13:12 -0500

    add echo

--- bin/backup/backup_10.sh
+++ bin/backup/backup_10.sh
@@ -4,10 +4,12 @@ echo "backup_10.sh"
 date "+%Y-%m-%d:%H:%M:%S"
 # copies encrypted doc.sparsebundle/ into 
 if [ -d /Volumes/10 ] && [ -d /Users/otter/Documents/doc.sparsebundle ] ; then
+	echo "backing up doc.sparsebundle to 10/"
 	/usr/bin/rsync -av --del /Users/otter/Documents/doc.sparsebundle/ /Volumes/10/doc_backup/doc.sparsebundle 
 fi
 # copies HDD "10" to WD4T
 if [ -d /Volumes/WD4T/HD0 ] && [ -d /Volumes/10 ] ; then
+	echo "backing up 10/ to WD4T"
 	/usr/bin/rsync -av --del /Volumes/10/ /Volumes/WD4T/HD0 
 fi
 # if [ -d /Volumes/WD4T/HD0 ] && [ -d /Users/otter/Documents/doc.sparsebundle ] ; then

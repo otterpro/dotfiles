51f8aa79762fb04875310f90c2ea5acad10aab74
otter.pro <coconutstudio@yahoo.com>
2020-01-04 22:05:26 -0500

    add backup for 10b1, 10b2

--- bin/backup/backup_10b2.sh
+++ bin/backup/backup_10b2.sh
@@ -1,16 +1,11 @@
 #!/bin/sh
 echo "======================"
-echo "backup_10.sh"
+echo "backup_10b2.sh"
 date "+%Y-%m-%d:%H:%M:%S"
-# copies encrypted doc.sparsebundle/ into 
-if [ -d /Volumes/10 ] && [ -d /Users/otter/Documents/doc.sparsebundle ] ; then
-	echo "backing up doc.sparsebundle to 10/"
-	/usr/bin/rsync -av --del /Users/otter/Documents/doc.sparsebundle/ /Volumes/10/doc_backup/doc.sparsebundle 
-fi
-# copies HDD "10" to WD4T
-if [ -d /Volumes/WD4T/10b1 ] && [ -d /Volumes/10 ] ; then
+# copies WD4T/10b1 to HDD/10b2
+if [ -d /Volumes/WD4T/10b1 ] && [ -d /Volumes/10b2 ] ; then
 	echo "backing up 10/ to WD4T"
-	/usr/bin/rsync -av --del /Volumes/10/ /Volumes/WD4T/10b1 
+	/usr/bin/rsync -av --del /Volumes/WD4T/10b1/ /Volumes/10b2 
 fi
 # if [ -d /Volumes/WD4T/HD0 ] && [ -d /Users/otter/Documents/doc.sparsebundle ] ; then
 # 	/usr/bin/rsync -av --del /Users/otter/Documents/doc.sparsebundle/ /Volumes/WD4T/HD0/doc_backup/doc.sparsebundle 

64c8353c5089c20485efcd9de8f4671323fe9ac2
otter.pro <coconutstudio@yahoo.com>
2020-01-15 20:16:09 -0500

    vscode setting update, add pushnotes.bat

--- bin/win/pushnotes.bat
+++ bin/win/pushnotes.bat
@@ -1,1 +1,13 @@
-%chocolateyToolsLocation%\cygwin\bin\bash.exe -c -l "/home/%username%/bin/pushnotes.sh >> /home/%username%/log.txt 2>&1"
+echo off
+@REM %chocolateyToolsLocation%\cygwin\bin\bash.exe -c -l "/home/%username%/bin/pushnotes.sh >> /home/%username%/log.txt 2>&1"
+rem GIT=/usr/bin/git
+rem #DATE=/usr/bin/date
+rem DATE=`env date`
+rem cd ~/notes
+rem $GIT add -A
+rem #$GIT commit -am"daily update `date`"
+rem #$GIT commit -am"daily update from Macbook Pro $DATE"
+rem $GIT commit -am"daily update from $OSTYPE $DATE"
+rem $GIT pull --no-edit
+rem $GIT push
+

7689296811b1c4ab29132eb7b9c60f683976ce89
otter.pro <coconutstudio@yahoo.com>
2021-11-22 21:08:48 -0500

    relocate files

--- win/bin/start/start-wsl.bat
+++ win/bin/start/start-wsl.bat
@@ -1,1 +1,8 @@
-wsl.exe ~
+@echo off
+REM start WSL2
+wsl.exe --status
+
+REM WAIT until WSL2 has finished loading...sleep 5 sec, cannot skip using key, and don't show prompt
+timeout /T 5 /NOBREAK > NUL
+
+REM launch anything that depends on wsl2

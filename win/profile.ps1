04ca94a97627cc3e32f76a887fc12205ef22bc64
otter.pro <coconutstudio@yahoo.com>
2021-11-22 20:56:08 -0500

    move from _win to win

---
+++ win/profile.ps1
@@ -0,0 +1,34 @@
+#profile for powershell
+# should be $ `$profile.CurrentUserAllHosts`
+# `C:\Users\USERNAME\Documents\WindowsPowerShell\profile.ps1`
+#function Get-GitStatus { git status $args }
+# New-Alias -Name gs -Value Get-GitStatus
+
+#function Set-GitCommit { git commit -am $args }
+#New-Alias -Name gc -Value Set-GitCommit
+
+
+function gs { git status  }
+function gpl { git pull }
+function gps { git push }
+function gadd { git add -A }
+function gcm { git commit -m $args}
+
+# function v {gvim $args}
+# -p --remote... allows opening text file into same instance with new tabs
+#function oldv {gvim.exe -p --remote-tab-silent $args}
+function v {
+    if ($args.count) {
+        gvim.exe -p --remote-tab-silent $args
+    }
+    else {
+        gvim.exe
+    }
+}
+
+# emacs or console vim for now?
+function e { vim.exe $args}
+
+# neovim implementations, flavor of the day
+# nvim-qt, neovide, etc...
+function n { nvim-qt.exe $args}

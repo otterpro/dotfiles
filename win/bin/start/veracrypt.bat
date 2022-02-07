307a697cca18bad678b3dc187de6115690837459
otter.pro <coconutstudio@yahoo.com>
2022-02-06 23:11:28 -0500

    add veracrypt, update dz60

---
+++ win/bin/start/veracrypt.bat
@@ -0,0 +1,5 @@
+REM mount veracrypt drive in K:
+"c:\Program Files\VeraCrypt\VeraCrypt.exe" /q /l k /v %USERPROFILE%\Documents\enc
+
+REM unmount H
+REM "c:\Program Files\VeraCrypt\VeraCrypt.exe" /q /d h

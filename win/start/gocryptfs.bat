4d3ae93c91b62bad157d2a24f626c38c5df86d94
otter.pro <coconutstudio@yahoo.com>
2022-12-14 21:59:44 -0500

    update gocryptfs

--- win/start/gocryptfs.bat
+++ win/start/gocryptfs.bat
@@ -3,7 +3,7 @@
 REM #### gocryptfs / cppcryptfs
 REM *** use GUI's automount to load the encrypted drive using saved password, no need to use param if using AutoMount
 REM pushd; try {...} # if using powershell
-IF EXIST c:\app\cppcryptfs1.4.3.11\cppcryptfs.exe (
-	start c:\app\cppcryptfs1.4.3.11\cppcryptfs.exe --tray
+IF EXIST c:\app\cppcryptfs1.4.4.0\cppcryptfs.exe (
+	start c:\app\cppcryptfs1.4.4.0\cppcryptfs.exe --tray
 ) 
 

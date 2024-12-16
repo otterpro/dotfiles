03a701b1cf2645271694180f2705a28c41e90d8d
otter.pro <coconutstudio@yahoo.com>
2024-12-16 10:16:12 -0500

    updated monitor-switch-*.bat to latest monitor setup

--- win/monitor-switch-home.bat
+++ win/monitor-switch-home.bat
@@ -3,8 +3,10 @@ rem controlmymonitor.exe /SetValue "HWP3117" 60 15
 rem VCP code 60 : Input switch
 rem * 15 = DP
 rem * 17 = HDMI
-controlmymonitor.exe /SetValue "DELA1C5" 60 17
-controlmymonitor.exe /SetValue "DELA1C4" 60 17
+SET DisplayPort=15
+SET HDMI=17
+controlmymonitor.exe /SetValue "DELA1C5" 60 %DisplayPort%
+controlmymonitor.exe /SetValue "DELA1C4" 60 %DisplayPort%
 rem controlmymonitor.exe /SetValue "DELA1C4" 60 15
 rem controlmymonitor.exe /SetValue "DELA1C4" 60 15
 
--- win/monitor-switch-work.bat
+++ win/monitor-switch-work.bat
@@ -3,5 +3,9 @@ rem controlmymonitor.exe /SetValue "HWP3117" 60 15
 rem VCP code 60 : Input switch
 rem * 15 = DP
 rem * 17 = HDMI
-controlmymonitor.exe /SetValue "DELA1C5" 60 15
-controlmymonitor.exe /SetValue "DELA1C4" 60 15
+SET DisplayPort=15
+SET HDMI=17
+controlmymonitor.exe /SetValue "DELA1C5" 60 %HDMI%
+controlmymonitor.exe /SetValue "DELA1C4" 60 %HDMI%
+rem controlmymonitor.exe /SetValue "DELA1C5" 60 15
+rem controlmymonitor.exe /SetValue "DELA1C4" 60 15

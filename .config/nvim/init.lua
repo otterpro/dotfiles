f8f6c2b448442bb16a61fe150ab970dc21dbbf6d
otter.pro <coconutstudio@yahoo.com>
2025-02-07 13:51:05 -0500

    update

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,2 +1,13 @@
-local vimrc = vim.fn.stdpath("config") .. "/vimrc"
+-- load legacy vim
+-- local vimrc = vim.fn.stdpath("config") .. "\\load_vim_config.vim"
+-- local vimrc = vim.fn.stdpath("config") .. "/vimrc.vim"
+--local vimrc = vim.fn.stdpath("config")
+-- vim.cmd.source(vimrc)
+-- vim.cmd.source(gvimrc)
+
+-- local vimrc = vim.fn.stdpath("config") .. "/vimrc.vim"
+-- vim.cmd.source(vimrc)
+
+local vimrc = vim.fn.stdpath("config") .. "/load_vim_config.vim"
 vim.cmd.source(vimrc)
+-- vim.cmd.source('/load_vim_config.vim')
---
+++ .config/nvim/load_vim_config.vim
@@ -0,0 +1,5 @@
+" load legacy vim
+set runtimepath^=~/.vim runtimepath+=~/.vim/after
+let &packpath = &runtimepath
+source ~/.vim/vimrc
+source ~/.vim/gvimrc

f7c22fbb028436eabe8f36c2ac216de56e69e5f0
otter.pro <coconutstudio@yahoo.com>
2025-02-07 21:35:44 -0500

    added legacy vimrc

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,13 +1,5 @@
 -- load legacy vim
--- local vimrc = vim.fn.stdpath("config") .. "\\load_vim_config.vim"
--- local vimrc = vim.fn.stdpath("config") .. "/vimrc.vim"
---local vimrc = vim.fn.stdpath("config")
--- vim.cmd.source(vimrc)
--- vim.cmd.source(gvimrc)
 
--- local vimrc = vim.fn.stdpath("config") .. "/vimrc.vim"
--- vim.cmd.source(vimrc)
-
-local vimrc = vim.fn.stdpath("config") .. "/load_vim_config.vim"
+-- local vimrc = vim.fn.stdpath("config") .. "/load_vim_config.vim"
+local vimrc = vim.fn.stdpath("config") .. "/legacy-vimrc.vim"
 vim.cmd.source(vimrc)
--- vim.cmd.source('/load_vim_config.vim')
--- .config/nvim/load_vim_config.vim
+++ .config/nvim/load_vim_config.vim
@@ -1,5 +1,9 @@
 " load legacy vim
 set runtimepath^=~/.vim runtimepath+=~/.vim/after
 let &packpath = &runtimepath
-source ~/.vim/vimrc
-source ~/.vim/gvimrc
+
+" ### this is a copy of vimrc but is meant to be slowly migrated to init.lua
+source legacy-vimrc
+" source ~/.vim/vimrc
+" ### since gvimrc is no longer used, it is commented out
+" source ~/.vim/gvimrc
